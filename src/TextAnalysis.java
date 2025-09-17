dbe280034edc9011f8525f43478ccfd1391be0ec
Sion Yap <sionpeterson@gmail.com>
2025-09-17 08:51:14 -0400

    field, constructor, and object creation

--- src/TextAnalysis.java
+++ src/TextAnalysis.java
@@ -1,10 +1,20 @@
 public class TextAnalysis // main class
 {
+    private String article1;
+    private String article2;
+    private String article3;
+
+    public TextAnalysis(String article1, String article2, String article3)
+    {
+        this.article1 = article1;
+        this.article2 = article2;
+        this.article3 = article3;
+    }
+
     public static void main(String[] args) // main method
     {
-
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        TextAnalysis a1 = new TextAnalysis("article 1", "article 2", "article 3");
+        // think about how to import article text files into JRE
 
         while(true) // menu loop
         {
