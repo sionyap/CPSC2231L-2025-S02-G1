4efb4968c1034e13ba3700d7756f0733e60bb310
Sion Yap <sionpeterson@gmail.com>
2025-09-17 08:21:19 -0400

    git push demonstration

---
+++ src/TextAnalysis.java
@@ -0,0 +1,25 @@
+public class TextAnalysis // main class
+{
+    public static void main(String[] args) // main method
+    {
+
+        while(true) // menu loop
+        {
+            // TODO : recall accepting user input without creating a new obj every time
+            menu(userChoice);
+        }
+
+    }
+
+    public static boolean menu(int option)
+    {
+        // TODO : recall java case switch syntax
+        switch(option)
+        {
+            case 1:
+
+        }
+
+    }
+
+}
